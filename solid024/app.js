0fe8049860db91fd26616eb91f40da9510651c97
Normaneil E. Macutay <normaneil.macutay@gmail.com>
2022-11-11 10:49:22 +0800

    Added solid024 directory

---
+++ solid024/app.js
@@ -0,0 +1,33 @@
+// Replace the DASH and HLS URIs when you test your own content.
+let myData = document.getElementById("browserCheckResult");
+let hlsUri = myData.getAttribute("data-hls-uri");
+
+// VideoJS
+const player = videojs("videojs-player", {
+    muted: true,
+    autoplay: true,
+    controlBar: {
+        fullscreenToggle: false,
+    },
+    userActions: {
+        doubleClick: false,
+    },
+});
+
+function configureDRM() {
+    player.ready(function () {
+        let playerConfig;
+        playerConfig = {
+            src: hlsUri,
+            type: 'application/x-mpegurl'
+        };
+        console.log("playerConfig", playerConfig);
+        player.src(playerConfig);
+    });
+}
+
+player.ready(function () {
+    configureDRM();
+});
+
+player.play();
